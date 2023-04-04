82cbb73224cbafd79ae6269f61dcaf79722751b1
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2023-04-04 16:35:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return <Router>
     <Header/>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="netflixClone/" element={<Home />} />
     </Routes>
     </Router>
 }
